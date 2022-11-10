f30de26745626dc1f3073117088d58fe5f67fae1
a.benkov <a.benkov@bars.group>
2022-11-11 00:18:13 +0700

    Верстака Добавил MainViewModel

---
+++ PhotoSorter/Views/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace PhotoSorter.Views
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _sourceFolderText;
+        public string SourceFolderText
+        {
+            get { return _sourceFolderText; }
+            set
+            {
+                if (value != _sourceFolderText)
+                {
+                    _sourceFolderText = value;
+                    OnPropertyChanged("SourceFolderText");
+                }
+            }
+        }
+
+        private string _destinationFolderText;
+        public string DestinationFolderText
+        {
+            get { return _destinationFolderText; }
+            set
+            {
+                if (value != _destinationFolderText)
+                {
+                    _sourceFolderText = value;
+                    OnPropertyChanged("DestinationFolderText");
+                }
+            }
+        }
+    }
+}
