6121128e08bed735c4a7ef5e5d524bb37feb96ae
a.benkov <a.benkov@bars.group>
2022-11-12 00:49:58 +0700

    Диалог для выбора папок к кнопкам Команды.

---
+++ PhotoSorter/Commands/DestinationFolderCommand.cs
@@ -0,0 +1,43 @@
+﻿using PhotoSorter.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace PhotoSorter.Commands
+{
+    public class DestinationFolderCommand : RoutedCommand
+    {
+        private MainWindow _mainWindow;
+        private MainViewModel _mainWiewModel;
+        public DestinationFolderCommand(MainWindow mainWindow, MainViewModel mainWiewModel)
+        {
+            _mainWindow = mainWindow;
+            _mainWiewModel = mainWiewModel;
+            _mainWindow.CommandBindings.Add(new CommandBinding(this, ExecutedCustomCommand, CanExecuteCustomCommand));
+        }
+
+        private void ExecutedCustomCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+
+                    _mainWiewModel.DestinationFolderText = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void CanExecuteCustomCommand(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+    }
+}
--- PhotoSorter/Views/MainViewModel.cs
+++ PhotoSorter/Views/MainViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System.ComponentModel;
+﻿using PhotoSorter.Commands;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace PhotoSorter.Views
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private MainWindow _mainWindow;
+
         public event PropertyChangedEventHandler? PropertyChanged;
+        public static SourceFolderCommand SelectSourceFolderCommand { get; private set; }
+        public static DestinationFolderCommand SelectDestinationFolderCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            _mainWindow = (MainWindow)App.Current.MainWindow;
+            SelectSourceFolderCommand = new SourceFolderCommand(_mainWindow, this);
+            SelectDestinationFolderCommand = new DestinationFolderCommand(_mainWindow, this);
+        }
 
         protected void OnPropertyChanged(string propertyName)
         {
@@ -33,7 +46,7 @@ namespace PhotoSorter.Views
             {
                 if (value != _destinationFolderText)
                 {
-                    _sourceFolderText = value;
+                    _destinationFolderText = value;
                     OnPropertyChanged("DestinationFolderText");
                 }
             }
